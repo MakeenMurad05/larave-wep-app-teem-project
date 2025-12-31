3038b9e2bbefb0dbc299e24491729f831f8a093b
MakeenMurad05 <makeenels5@gmail.com>
2025-12-31 23:36:04 +0200

    Update User model and debugbar gitignore

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable , HasRoles;
+    
 
     /**
      * The attributes that are mass assignable.
@@ -22,7 +27,7 @@ class User extends Authenticatable
         'email',
         'password',
         'is_active',
-        'user_type'
+        'user_type',
     ];
 
     /**
@@ -45,9 +50,15 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_active' => 'boolean',
         ];
     }
 
+    
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->user_type === 'Admin';
+    }
 
     public function profile()
     {
@@ -68,4 +79,6 @@ class User extends Authenticatable
     {
         return $this->hasMany(ActivityLog::class, 'causer_id');
     }
+
+   
 }
