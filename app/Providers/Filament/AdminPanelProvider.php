c3d9f931290fe07ce8fd7db68dc0355f961952b3
Omar Alkaseh <youremail@gmail.com>
2026-01-07 21:07:08 +0200

    Update Route Name to Task_Management

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('Task_Management')
             ->login()
             ->registration(MyRegister::class)
             ->colors([
