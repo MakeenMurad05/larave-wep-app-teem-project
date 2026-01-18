9181392fc4d5f341ff31f33102668e31a32b0119
Omar Alkaseh <youremail@gmail.com>
2026-01-18 12:24:38 +0200

    update user view

--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -6,6 +6,8 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Section;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
@@ -43,7 +45,41 @@ class UsersTable
             ])
             ->recordActions([
                 EditAction::make(),
-                ViewAction::make(),
+                ViewAction::make()
+                ->form([
+                    
+                // القسم الأول: معلومات الحساب الأساسية
+                Section::make('Account Information')
+                    ->schema([
+                        \Filament\Forms\Components\TextInput::make('name'),
+                        \Filament\Forms\Components\TextInput::make('email'),
+                        \Filament\Forms\Components\TextInput::make('user_type'),
+                    ])->columns(2),
+
+                // القسم الثاني: معلومات البروفايل المرتبطة
+                Section::make('Profile Details')
+                    ->schema([
+                        \Filament\Forms\Components\FileUpload::make('profile.photo')
+                            ->label('Profile Photo')
+                            ->avatar()
+                            ->disk('public'), // تأكد من استخدام نفس القرص
+
+                Group::make([
+                    \Filament\Forms\Components\TextInput::make('profile.first_name')
+                        ->label('First Name'),
+                    \Filament\Forms\Components\TextInput::make('profile.last_name')
+                        ->label('Last Name'),
+                    \Filament\Forms\Components\TextInput::make('profile.phone')
+                        ->label('Phone Number'),
+                    \Filament\Forms\Components\DatePicker::make('profile.birth_date')
+                        ->label('Birth Date'),
+                ])->columns(2),
+
+                \Filament\Forms\Components\Textarea::make('profile.bio')
+                    ->label('Bio')
+                    ->columnSpanFull(),
+            ])
+    ]),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
