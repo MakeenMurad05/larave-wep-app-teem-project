4bf30a66abdae397164e785a32931b462c7be7fa
Omar Alkaseh <youremail@gmail.com>
2026-01-18 18:16:50 +0200

    update user view

--- app/Filament/Pages/EditProfile.php
+++ app/Filament/Pages/EditProfile.php
@@ -118,7 +118,6 @@ class EditProfile extends Page implements HasForms
     {
         $data = $this->form->getState();
         $user = Auth::user();
-dd(config('filesystems.disks.public.root'));
 
         $photoPath = is_array($data['photo']) ? array_values($data['photo'])[0] : $data['photo'];
 
