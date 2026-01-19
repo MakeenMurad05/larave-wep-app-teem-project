929cd5b3738e574571b383de15c99aef473e4b5c
Omar Alkaseh <youremail@gmail.com>
2026-01-19 08:54:19 +0200

    update user view

--- app/Filament/Resources/Tasks/Schemas/TaskForm.php
+++ app/Filament/Resources/Tasks/Schemas/TaskForm.php
@@ -34,8 +34,7 @@ class TaskForm
                     'blocked' => 'Blocked',
                     'completed' => 'Completed',
                 ])
-                ->required()
-                ->visible(fn () => auth()->user()->hasRole('Member')),
+                ->required(),
 
             Select::make('project_id')
                 ->relationship(
